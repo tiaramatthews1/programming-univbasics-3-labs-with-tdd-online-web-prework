534aa7cc4fa8aaff7387397702076f572a3d3764
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-06-27 18:35:09 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,3 @@
-# Add your variables here
+first_number = 1.0
+second_number = 2.0
+sum === first_number + second_number
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -47,3 +47,4 @@ describe "./calculator.rb" do
     expect(quotient).to eq(first_number/second_number)
   end
 end
+
