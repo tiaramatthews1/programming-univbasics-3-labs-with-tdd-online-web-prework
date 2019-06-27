3512ec2673162239ff8ffd70d3c1b6aa35a115b1
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-06-27 18:42:28 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-# Add your variables here
+first_number = 1.0
+second_number = 2.0
+sum === first_number + second_number
+difference === first_number - second_number
+product === first_number * second_number
+quotient === first_number / second_number
